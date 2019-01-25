19e7b3065c9875062b98a889b163665440a0db06
Josue Abraham <josuea@noukod.com>
2019-01-25 19:49:41 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: { scope: :release_year }
   with_options if: :is_released? do |release|
-    release.validates :release_year, presence: true, numericality: { less_than: Date.current.year }
+  release.validates :release_year, presence: true, numericality: { less_than: Date.current.year }
   end
 
   def is_released?
